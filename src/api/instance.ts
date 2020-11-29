edf1cf57e3012743b6d8b2222bd42b9e9901d376
kacperkapusciak <kapusciak.k@gmail.com>
2020-11-29 16:36:23 +0100

    feat: configure app for production

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
+const dev = 'http://localhost:7000';
+const prod = 'https://recomvino-be.herokuapp.com';
+const url = process.env.NODE_ENV === 'production' ? prod : dev;
+
 export default axios.create({
-  baseURL: 'http://localhost:7000/api/',
+  baseURL: `${url}/api/`,
 });
